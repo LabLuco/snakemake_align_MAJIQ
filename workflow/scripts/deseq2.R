867e8c8411c1cf664d76d4a2cc90fb041824a93f
ElsaClaude <claudeelsa@outlook.fr>
2021-04-09 10:49:59 +0200

    update call deseq2 library

--- workflow/scripts/deseq2.R
+++ workflow/scripts/deseq2.R
@@ -1,12 +1,7 @@
 setwd("./../../results/Diff_Exp/")
 
-library(S4Vectors, lib.loc='/usr/local/lib/R/site-library/')
-library(BiocGenerics, lib.loc='/usr/local/lib/R/site-library/')
-require(devtools)
-if (identical(require("S4Vectors"),FALSE)) {BiocManager::install('S4Vectors')}
-if (identical(packageVersion('S4Vectors')>='0.12.2',FALSE)) {BiocManager::install('S4Vectors')}
-install_version("S4Vectors", version = "0.24.4", repos = "http://cran.us.r-project.org")
 library(DESeq2, lib.loc='/usr/local/lib/R/site-library/')
+
 sampletable <- read.table("samplesheet.tsv", header=T, sep="\t")
 rownames(sampletable) <- sampletable$sample_name
 
