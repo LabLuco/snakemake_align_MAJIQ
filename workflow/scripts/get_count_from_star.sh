d33fe45aa40b3476aac4e14855cecd588396a936
ElsaClaude <claudeelsa@outlook.fr>
2021-04-26 18:09:55 +0200

    update bash commands to get deseq2 rawcount tsv

--- workflow/scripts/get_count_from_star.sh
+++ workflow/scripts/get_count_from_star.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 gtf=$1
 paste ../../results/alignment/*/*_ReadsPerGene.out.tab | grep -v "_" | awk '{{printf "%s\t", $1}}{{for (i=4;i<=NF;i+=4) printf "%s\t", $i; printf "\n" }}' > ../../results/Diff_Exp/tmp
-sed -e "1igene_name\t$(ls ../../results/alignment/*/*_ReadsPerGene.out.tab | tr '\n' '\t' | sed 's/_ReadsPerGene.out.tab//g')" ../../results/Diff_Exp/tmp | cut -f1-7 > ../../results/Diff_Exp/raw_counts_matrix.tsv
-rm ../../results/Diff_Exp/tmp
+echo -n -e "gene_name\t" > ../../results/Diff_Exp/tmpheader| for file in ../../results/alignment/*/*_ReadsPerGene.out.tab; do basename $file _ReadsPerGene.out.tab | tr '\n' '\t' >> ../../results/Diff_Exp/tmpheader ; done
+echo -n -e "\n" >> ../../results/Diff_Exp/tmpheader
+sed -i -e '1 { r ../../results/Diff_Exp/tmpheader' -e 'N; }' ../../results/Diff_Exp/tmp
+mv ../../results/Diff_Exp/tmp ../../results/Diff_Exp/raw_counts_matrix.tsv
+rm ../../results/Diff_Exp/tmpheader
 cat ${gtf} | awk -F "\t" 'BEGIN{{OFS="\t"}}{{if($3=="transcript"){{split($9, a, "\""); print a[4],a[2],a[8]}}}}' > ../../results/Diff_Exp/tx2gene.tsv
